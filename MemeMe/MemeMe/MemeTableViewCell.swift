fd5c3fea15aa15b4403c220efa49f4d92781a674
coltonballinger <coltonballinger@yahoo.com>
2015-04-28 12:48:47 -0400

    Committed by Udacity

---
+++ MemeMe/MemeMe/MemeTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMe
+//
+//  Created by Ballinger, Colton J. on 4/16/15.
+//  Copyright (c) 2015 Ballinger, Colton J. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+    @IBOutlet weak var memeNameLabel: UILabel!
+}
